dddafd72fc05d9f6523d075d5ae558cbe264b929
david323902 <80213991+david323902@users.noreply.github.com>
2026-03-18 11:14:04 -0500

    MODIFIACION PARA QUITAR EL LOGOO

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL=https:https://tienda-online-backend-xu3j.onrender.com
+  baseURL: "https://tienda-online-backend-xu3j.onrender.com/api"
 });
 
 export default api;
